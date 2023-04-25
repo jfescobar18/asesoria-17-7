1bbded999c5df66f11f6d3e4aa47c3b8285f4f2b
Francisco Escobar <jfescobar18@hotmail.com>
2023-04-25 14:07:03 -0600

    update

--- ejercicio.js
+++ ejercicio.js
@@ -1,26 +1,33 @@
-var elementos = [];
+var elementos = []
+
+function mostrarElementos() {
+    const elementosString = elementos
+        .map((numero) => {
+            return `- ${numero}`
+        })
+        .join("\n")
+
+    // [1,3,4,5].join("\n") => "1,2,3,4,5" => 
+    // 1
+    // 2
+    // 3 ...
+    // \n = salto de linea
+    alert(elementosString)
+}
 
 function agregarElemento() {
-    const elemento = prompt("Ingresa un numero");
+    const elemento = prompt("Ingresa un numero")
     // push sirve para agregar elementos a un arreglo de izquerda a derecha
-    elementos.push(elemento);
+    elementos.push(elemento)
 
-    mostrarElementos();
+    mostrarElementos()
 }
 
 function eliminarElemento() {
-    const elemento = prompt("Ingresa el numero que quieres eliminar");
+    const elemento = prompt("Ingresa el numero que quieres eliminar")
     elementos = elementos.filter((numero) => {
-        return numero != elemento;
-    });
+        return numero != elemento
+    })
 
-    mostrarElementos();
-}
-
-function mostrarElementos() {
-    const elementosString = elementos.map((numero) => {
-        return `- ${numero}`;
-    }).join("\n");
-    // \n = salto de linea
-    alert(elementosString);
+    mostrarElementos()
 }
